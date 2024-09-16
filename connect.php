15f162c490762be21014a91754d453b144deb362
Cacton12 <Colbyacton12@icloud.com>
2024-09-16 15:02:31 -0300

    Finished part 1 of sprint 1
    renamed __Y__ to y-ColbyActon
    changed database connection in connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'y');
+define('DB_NAME', 'y-ColbyActon');
 	
 global $con;
 	  $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS, DB_NAME);
