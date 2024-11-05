fc29decdab658daecde01562e01dd7b4d57cc111
Cacton12 <Colbyacton12@icloud.com>
2024-11-05 10:31:20 -0400

    search.php worked on

--- Search.php
+++ Search.php
@@ -1,3 +1,25 @@
 <?php
 echo "COMING SOON";
+/*3 types of vulnerabilities
+1. software(servers, programming languages. OS etc.)
+2. Poorly protected data (DB protected by a weak password)
+ admin console wide open with easy url
+ 3. User input- Hackers will explor vulnerabilities due to clumsy 
+*/
+include("connect.php");
+$searchTerm = "%" . $_GET["search"] . "%";
+// $sql = "SELECT * FROM users WHERE first_name LIKE '%$searchTerm%' OR last_name LIKE '%$searchTerm%'";
+$sql = "select * from users where first_name like ? or last_name like ?";
+$stmt = $con->prepare($sql);
+$stmt->bind_param("ss",  $searchTerm, $searchTerm);
+$stmt->execute();
+$result = $stmt->get_result();
+while ($row= $result->fetch_assoc()) {
+    echo $row['first_name'] . " " . $row['last_name'] . "<br>";
+}
+echo $sql . "<br>";
+$stmt->close();
+$con->close();
+
+// echo phpinfo();
 ?>
